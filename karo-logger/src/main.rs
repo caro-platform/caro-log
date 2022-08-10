4892fffa994d411b49df06a398976211d47ed6cc
Alexander Smoktal <cosm.ua@gmail.com>
2022-08-10 15:23:17 +0300

    Implement log rotation.

--- karo-logger/src/main.rs
+++ karo-logger/src/main.rs
@@ -1,43 +1,34 @@
+mod args;
 mod logger;
+mod rotator;
 
-use clap::{self, Parser};
+use clap::Parser;
 use log::*;
 
 use karo_bus_lib::Bus;
 
-use karo_log_common::{
-    log_message::LogMessage, DEFAULT_LOG_LOCATION, LOGGING_METHOD_NAME, LOGGING_SERVICE_NAME,
-};
+use karo_log_common::{log_message::LogMessage, LOGGING_METHOD_NAME, LOGGING_SERVICE_NAME};
 use karo_log_lib::Logger as LibLogger;
 
 use logger::Logger;
 
-/// Karo bus monitor
-#[derive(Parser, Debug)]
-#[clap(version, about, long_about = None)]
-pub struct Args {
-    /// Log level: OFF, ERROR, WARN, INFO, DEBUG, TRACE
-    #[clap(short, long, value_parser, default_value_t = LevelFilter::Info)]
-    pub log_level: log::LevelFilter,
-
-    /// Service to monitor
-    #[clap(long, value_parser, default_value_t = DEFAULT_LOG_LOCATION.into())]
-    pub log_location: String,
-}
+const LOG_ROTATED_SIGNAL: &str = "log_rotated";
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Starting Karo logging service");
 
-    let args = Args::parse();
+    let args = args::Args::parse();
 
     let _ = LibLogger::new(args.log_level, true);
-
-    let logger = Logger::new(&args.log_location);
 
     let mut bus = Bus::register(LOGGING_SERVICE_NAME)
         .await
         .expect("Failed to register logging service");
+
+    let rotated_signal = bus.register_signal::<String>(LOG_ROTATED_SIGNAL).unwrap();
+
+    let logger = Logger::new(&args, rotated_signal);
 
     bus.register_method(LOGGING_METHOD_NAME, move |message: LogMessage| {
         let mut logger = logger.clone();
